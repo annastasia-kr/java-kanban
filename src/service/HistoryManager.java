34adfa55a621fabeec8f912c4ee6e760280c0f2d
avkrasnova <avkrasn0va@yandex.ru>
2025-01-29 16:41:02 +0300

    Codestyle

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -5,8 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     void removeAll(Map<Integer, ? extends Task> map);
+
     List<Task> getHistory();
+
 }
